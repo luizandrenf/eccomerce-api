ba1b67508750100c9aca6531a9998540ce4cf37f
luizandrenf <luizandrenf@gmail.com>
2024-10-04 18:11:24 -0400

    docs: sweagger OpenAPI to docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API Documentation')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
